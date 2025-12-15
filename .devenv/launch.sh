bafd922a9f33939e41cbb140fdc27587046c4091
Tongxi Lou <tongxi.lou@gmail.com>
2025-12-15 21:13:39 +0100

    fix docker compose

--- .devenv/launch.sh
+++ .devenv/launch.sh
@@ -2,22 +2,19 @@
 
 # Get the directory where the script itself is located
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+WS_DIR="$(dirname ${SCRIPT_DIR})"
 
-cd "$SCRIPT_DIR" || exit
-
-echo "Now in parent folder: $(pwd)"
-
-# Name of the service defined in docker-compose.yml
-SERVICE_NAME="mlfield"
+cd ${WS_DIR}
 
 # Export UID/GID for Compose
-export UID=$(id -u)
-export GID=$(id -g)
+export HOST_UID="$(id -u)"
+export HOST_GID="$(id -g)"
+export USERNAME="$(whoami)"
 
-export WS_DIR=$(realpath "$(pwd)")
+export WS_DIR=${WS_DIR}
 
-# Build and start the container
-docker compose up -d --build
+# Build the container
+docker compose -f .devenv/docker-compose.yml build
 
-# # Attach to the container shell
-docker compose exec $SERVICE_NAME bash
+# Attach to the container shell
+docker compose -f .devenv/docker-compose.yml run -ti -v $(pwd):/workspace ml_devenv bash
