aad39c63253174ae75e6cf961a217f04957e3a2e
Tongxi Lou <tongxi.lou@gmail.com>
2025-12-22 19:20:49 +0100

    do the copilot day1 suggestions

---
+++ ssl/day1/prep.py
@@ -0,0 +1,23 @@
+import torchvision.transforms as T
+from torchvision.datasets import CIFAR10
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+
+transform = T.Compose(
+    [T.RandomResizedCrop(32), T.RandomHorizontalFlip(), T.ColorJitter(0.4, 0.4, 0.4, 0.1), T.ToTensor()]
+)
+
+dataset = CIFAR10(root="./data", train=True, download=True, transform=transform)
+loader = DataLoader(dataset, batch_size=8, shuffle=True)
+
+images, labels = next(iter(loader))
+
+grid = images.permute(0, 2, 3, 1)
+plt.figure(figsize=(10, 3))
+for i in range(8):
+    plt.subplot(1, 8, i + 1)
+    plt.imshow(grid[i])
+    plt.axis("off")
+
+# plt.show()
+plt.savefig("image.png")
