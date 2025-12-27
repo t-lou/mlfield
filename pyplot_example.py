1d2f69c80b2965a9e6caa6212de28527b14a6ce8
Tongxi Lou <tongxi.lou@gmail.com>
2025-12-27 21:37:01 +0100

    add example for pyplot, not sure where to put yet

---
+++ pyplot_example.py
@@ -0,0 +1,15 @@
+# install the Jupyter plugin  (from Microsoft) inside the container,
+# then interactive mode should pop up
+
+# %% cell 1
+import matplotlib.pyplot as plt
+
+plt.plot([1, 2, 3])
+plt.show()
+
+# %% cell 2
+
+plt.plot([3, 2, 1])
+plt.show()
+
+# %%
