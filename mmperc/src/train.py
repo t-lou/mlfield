cba0bd809659150880f915507dec02b10b5f58e5
Tongxi Lou <tongxi.lou@gmail.com>
2026-01-26 20:09:00 +0100

    try to improve dataloader configs

--- mmperc/src/train.py
+++ mmperc/src/train.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 import torch.optim as optim
 from torch.utils.data import DataLoader
 
@@ -7,21 +9,50 @@ from model.simple_model import SimpleModel
 from pipeline.train_bbox2d import train_model
 
 
+@dataclass
+class TrainConfig:
+    # DataLoader
+    batch_size: int = 2
+    num_workers: int = 0
+    prefetch_factor: int = 2
+    pin_memory: bool = False
+    persistent_workers: bool = False
+    shuffle: bool = True
+
+
 def main():
     device = get_best_device()
-    batch_size = 2
+    settings = {
+        "lite": TrainConfig(
+            batch_size=2,
+            num_workers=1,
+            prefetch_factor=2,
+            pin_memory=False,
+            persistent_workers=False,
+            shuffle=True,
+        ),
+        "normal": TrainConfig(
+            batch_size=16,
+            num_workers=4,
+            prefetch_factor=2,
+            pin_memory=True,
+            persistent_workers=True,
+            shuffle=True,
+        ),
+    }
+    chosen_setting = settings["lite"]
 
     path_dataset = "/workspace/mmperc/data/a2d2"
     dataset = A2D2Dataset(root=path_dataset)
     dataloader = DataLoader(
         dataset,
-        batch_size=batch_size,
+        batch_size=chosen_setting.batch_size,
         shuffle=True,
         collate_fn=bev_collate,
-        num_workers=8,
-        pin_memory=True,
-        persistent_workers=True,
-        prefetch_factor=4,
+        num_workers=chosen_setting.num_workers,
+        pin_memory=chosen_setting.pin_memory,
+        persistent_workers=chosen_setting.persistent_workers,
+        prefetch_factor=chosen_setting.prefetch_factor,
     )
 
     model = SimpleModel().to(device)
